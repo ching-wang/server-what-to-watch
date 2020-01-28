72a79f9fc47c1a1010d3547448f2ca6fb187957d
Qing <chingwang.qw@gmail.com>
2020-01-28 22:26:55 +0000

    fixed relationship

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  belongs_to :director
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
--- db/migrate/20200127214007_create_wish_lists.rb
+++ db/migrate/20200127214007_create_wish_lists.rb
@@ -2,7 +2,7 @@ class CreateWishLists < ActiveRecord::Migration[6.0]
   def change
     create_table :wish_lists do |t|
       t.string :name
-      t.integer :user_id
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
