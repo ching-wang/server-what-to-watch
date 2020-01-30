ae29b05e5b44d259b12d22e51d50c312fdd8016c
Qing <chingwang.qw@gmail.com>
2020-01-30 22:08:32 +0000

    Force auth to work with fixed key

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
+    origins "*"
 
-    resource '*',
+    resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
- 
