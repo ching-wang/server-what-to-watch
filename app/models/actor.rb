2716c618d240f2e9ee9c723655149b94419e9514
Qing <chingwang.qw@gmail.com>
2020-02-02 11:37:43 +0000

    add actor model, controller and serializer

---
+++ app/models/actor.rb
@@ -0,0 +1,4 @@
+class Actor < ApplicationRecord
+  has_many :actor_movies
+  has_many :movies, through: :actor_movies
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   belongs_to :director
   has_many :movie_genres
   has_many :genres, through: :movie_genres
+  has_many :actor_movies
+  has_many :actors, through: :actor_movies
 end
