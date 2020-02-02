d5f828c4baffcbe0cfd9389ffc1b3849485066e9
Qing <chingwang.qw@gmail.com>
2020-02-02 18:51:03 +0000

    fix movie year atribute issue

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,5 +1,5 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :released, :language, :poster
+  attributes :id, :title, :year, :language, :poster
   belongs_to :director
   has_many :actor_movies
   has_many :actors, through: :actor_movies
