b45dc13a549ff779b6c907e44a5cc3ae21f5ec4a
Qing <chingwang.qw@gmail.com>
2020-02-04 20:29:48 +0000

    delete a wishlist without refereshing the page

--- app/controllers/api/v1/wish_lists_controller.rb
+++ app/controllers/api/v1/wish_lists_controller.rb
@@ -1,29 +1,65 @@
 class Api::V1::WishListsController < ApplicationController
+  def index
+    if !logged_in?
+      protected_action()
+      return
+    end
+
+    wish_lists = WishList.where("user_id = ?", @current_user.id)
+    render json: wish_lists
+  end
+
   def show
     wishList = WishList.find_by(id: params[:id])
     render json: wishList, include: [wish_list_items: [:movie]]
   end
 
   def create
-    wishList = WishList.find_or_create_by(params[:wishList_params])
+    if !logged_in?
+      protected_action()
+      return
+    end
+
+    wishList = WishList.new(wishList_params)
+    wishList.user_id = @current_user.id
+    wishList.save
     render json: { wishlist: WishListSerializer.new(wishList) }
   end
 
   def update
-    wishList = WishList.find_by(params[:wish_list_id])
+    if !logged_in?
+      protected_action()
+      return
+    end
+
+    wishList = WishList.find_by(params[:id])
     wishList.update(wishList_params)
     render json: { wishlist: WishListSerializer.new(wishList) }
   end
 
   def destroy
-    wishList = WishList.find_by(params[:wish_list_id])
-    deleteWishlist = wishList.destroy
-    render json: deleteWishlist
+    if !logged_in?
+      protected_action()
+      return
+    end
+
+    wish_list = WishList.find_by(id: params[:id])
+    if !wish_list
+      render json: { errors: ["No such wish list"] }, status: :not_found
+      return
+    end
+
+    if wish_list.user_id != @current_user.id
+      render json: { errors: ["You can't delete other people's wish lists!"] }, status: :unauthorized
+      return
+    end
+    deleted = wish_list.destroy
+    render json: deleted
   end
 
   private
 
   def wishList_params
-    params.require(:wishList).permit(:name, :description, :image)
+    params.permit(:name, :description, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :update, :destroy]
-      resources :wish_lists, only: [:create, :update, :destroy, :show]
+      resources :wish_lists
       resources :wish_list_items, only: [:create, :destroy]
       get "/movies/:imdb_id", to: "movies#show"
       post "/login", to: "users#login"
